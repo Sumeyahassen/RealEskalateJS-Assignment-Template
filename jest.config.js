febe5fcdd27c6168299581c65e2b1b09f0df35b2
sumeyahassen <sumeyahassensumi@gmail.com>
2026-02-17 04:14:52 +0300

    to add the vite and test one fail

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  testMatch: ['**/*.test.[jt]s?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
